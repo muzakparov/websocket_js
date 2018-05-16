70583f7f7a7c63429ffc0faf1e3c750784f1d2a5
muzakparov <muzakparov@gmail.com>
2018-05-16 11:56:12 +0300

    modernized

--- server.js
+++ server.js
@@ -1,27 +1,43 @@
-var express = require('express')
-// var sendJSON =require('./productionJSON')
-var sendJSON =require('./test')
+const express = require('express')
+const http = require('http')
+const WebSocket = require('ws')
 
-var WebSocketServer = require('ws').Server,
-    wss             = new WebSocketServer({port: 1984, path: "/api/settler/ws"})
+// let sendJSON =require('./productionJSON')
+let sendJSON =require('./test')
+var intervalListener
 
 sendJSON=JSON.stringify(sendJSON)
 
-var intervalListener
 
-wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-  })
+const app = express()
 
-  intervalListener=setInterval(
-    () => {
-    	console.log("polling...")
-    	
-    	ws.send(`${sendJSON}`)
-    },
-    1000
-  )
+//initialize a simple http server
+const server = http.createServer(app)
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', (ws) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    })
+
+    intervalListener=setInterval(
+	    () => {
+	    	console.log("polling...")
+
+	    	if (ws.readyState != WebSocket.CLOSED) { 
+				ws.send(`${sendJSON}`)
+	    	}
+	    	
+	    },
+	    1000
+    )
 })
 
 wss.on('close', function close() {
@@ -30,14 +46,7 @@ wss.on('close', function close() {
   clearInterval(intervalListener)
 })
 
-
-// var app = express()
-
-// app.get('/', function (req, res) {
-//     res.sendFile(__dirname + '/ws.html');
-// })
-
-// app.listen(1234, function () {
-//   console.log('Example app listening on port 1234!')
-// })
-
+//start our server
+server.listen(1984, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+})
